8b65f5076e76a04a3675bfad1cb7ddd058f458eb
zjl <364466198@qq.com>
2020-12-05 11:18:04 +0800

    make directory: build & update the README.md

---
+++ Task2/build/CMakeFiles/mycp.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zjl/ON_experiment/workbench/BasicKnowledge/Task2/src/my_copy.cpp" "/home/zjl/ON_experiment/workbench/BasicKnowledge/Task2/build/CMakeFiles/mycp.dir/src/my_copy.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Task2/build/CMakeFiles/mycp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mycp.dir/src/my_copy.cpp.o"
+  "mycp.pdb"
+  "mycp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mycp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
